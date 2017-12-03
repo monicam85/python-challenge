499efabaa1b7da822765bc1eba697633573dcb62
monicam85 <31086980+monicam85@users.noreply.github.com>
2017-12-02 23:47:34 -0800

    Add files via upload

--- Pyboss/main.py
+++ Pyboss/main.py
@@ -1,59 +1,57 @@
-us_state_abbrev = {
-        'AK': 'Alaska',
-        'AL': 'Alabama',
-        'AR': 'Arkansas',
-        'AS': 'American Samoa',
-        'AZ': 'Arizona',
-        'CA': 'California',
-        'CO': 'Colorado',
-        'CT': 'Connecticut',
-        'DC': 'District of Columbia',
-        'DE': 'Delaware',
-        'FL': 'Florida',
-        'GA': 'Georgia',
-        'GU': 'Guam',
-        'HI': 'Hawaii',
-        'IA': 'Iowa',
-        'ID': 'Idaho',
-        'IL': 'Illinois',
-        'IN': 'Indiana',
-        'KS': 'Kansas',
-        'KY': 'Kentucky',
-        'LA': 'Louisiana',
-        'MA': 'Massachusetts',
-        'MD': 'Maryland',
-        'ME': 'Maine',
-        'MI': 'Michigan',
-        'MN': 'Minnesota',
-        'MO': 'Missouri',
-        'MP': 'Northern Mariana Islands',
-        'MS': 'Mississippi',
-        'MT': 'Montana',
-        'NA': 'National',
-        'NC': 'North Carolina',
-        'ND': 'North Dakota',
-        'NE': 'Nebraska',
-        'NH': 'New Hampshire',
-        'NJ': 'New Jersey',
-        'NM': 'New Mexico',
-        'NV': 'Nevada',
-        'NY': 'New York',
-        'OH': 'Ohio',
-        'OK': 'Oklahoma',
-        'OR': 'Oregon',
-        'PA': 'Pennsylvania',
-        'PR': 'Puerto Rico',
-        'RI': 'Rhode Island',
-        'SC': 'South Carolina',
-        'SD': 'South Dakota',
-        'TN': 'Tennessee',
-        'TX': 'Texas',
-        'UT': 'Utah',
-        'VA': 'Virginia',
-        'VI': 'Virgin Islands',
-        'VT': 'Vermont',
-        'WA': 'Washington',
-        'WI': 'Wisconsin',
-        'WV': 'West Virginia',
-        'WY': 'Wyoming'
-}
+'''
+Author:
+Date: 11/21/2017
+Revision: -
+'''
+import os
+import re
+import csv
+import time
+from tkinter import filedialog
+from tkinter import Tk
+from tkinter import messagebox
+from us_state_abbrev import *
+
+def main():
+    CUR_DIR = os.getcwd()
+    root = Tk()
+    root.withdraw()
+    global start_time
+
+
+    messagebox.showinfo( \
+	   message='Please select input file(s). Use CTRL+Click to select multiple file', \
+           default='ok', title='Information')
+    # select file using file browser GUI
+    FILENAME_PATH =  filedialog.askopenfilenames(initialdir=CUR_DIR, title="Select file", filetypes=(("Comma Seperated Value Files","*.csv"),("all files","*.*")))
+    FILENAME_PATH = (list(FILENAME_PATH))
+
+    for fn in FILENAME_PATH:
+        # split string containing full path and filename
+        FILENAME_PATH_SPLIT = fn.split(sep = '/')
+        # define OUTPUT filename for FILENAME_PATH_SPLIT
+        FILENAME, EXT = (FILENAME_PATH_SPLIT[len(FILENAME_PATH_SPLIT) - 1]).split(sep = '.')
+        # append .out file extension. OUT = [orignal filename].out
+        OUT = os.path.join(CUR_DIR, FILENAME + '.out')
+        start_time= time.process_time()
+        with open(fn, 'r', newline = '' ) as INPUT_FILE:
+                READER_csv = csv.DictReader(INPUT_FILE)
+                with open(OUT, 'w', newline = '' ) as OUTPUT_FILE:
+                    WRITER_csv = csv.DictWriter(OUTPUT_FILE, fieldnames = READER_csv.fieldnames)
+                    WRITER_csv.writeheader()
+                    for row in READER_csv:
+                        #Update/reformat the current dictionary
+                        ROW_UPDATED=row.copy()
+                        ROW_UPDATED['Name'] = re.sub(' ', ',', ROW_UPDATED['Name'], count = 1, flags = 0)
+                        ROW_UPDATED['DOB'] = '/'.join(i for i in ROW_UPDATED['DOB'].split(sep='-')[::-1])
+                        ROW_UPDATED['SSN'] = re.sub(r'[0-9]{3}-[0-9]{2}', '***-**', ROW_UPDATED['SSN'], count = 0, flags = 0)
+                        ROW_UPDATED['State'] = list(us_state_abbrev.keys())[list(us_state_abbrev.values()).index(row['State'])]
+
+                        #Write updated dictionert to output file
+                        WRITER_csv.writerow(ROW_UPDATED)
+
+
+if __name__ == "__main__":
+    main()
+    Elasped=time.process_time()-start_time
+    print('Elasped time: %f' % (Elasped))
